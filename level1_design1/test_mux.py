121cda4ca0cf576115c0ea8d94a4a64022ecebba
arunimRay <arunimray06@gmail.com>
2022-07-20 15:53:09 +0000

    update test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,47 +7,56 @@ import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    
+    cocotb.log.info('##### CTB: Develop your test here ########')
      
-    inp = [
-    dut.inp0.value,
-    dut.inp1.value, 
-    dut.inp2.value, 
-    dut.inp3.value,
-    dut.inp4.value,
-    dut.inp5.value,
-    dut.inp6.value, 
-    dut.inp7.value,
-    dut.inp8.value,
-    dut.inp9.value, 
-    dut.inp10.value, 
-    dut.inp11.value, 
-    dut.inp12.value, 
-    dut.inp13.value, 
-    dut.inp14.value, 
-    dut.inp15.value, 
-    dut.inp16.value, 
-    dut.inp17.value, 
-    dut.inp18.value, 
-    dut.inp19.value,  
-    dut.inp20.value,
-    dut.inp21.value, 
-    dut.inp22.value, 
-    dut.inp23.value,
-    dut.inp24.value, 
-    dut.inp25.value, 
-    dut.inp26.value, 
-    dut.inp27.value, 
-    dut.inp28.value, 
-    dut.inp29.value, 
-    dut.inp30.value
-    ]
-     
-    randomlist = random.randint(0,3,31)
-    inp = randomList
+    for i in range(5):
+        inp = [
+        dut.inp0.value,
+        dut.inp1.value, 
+        dut.inp2.value, 
+        dut.inp3.value,
+        dut.inp4.value,
+        dut.inp5.value,
+        dut.inp6.value, 
+        dut.inp7.value,
+        dut.inp8.value,
+        dut.inp9.value, 
+        dut.inp10.value, 
+        dut.inp11.value, 
+        dut.inp12.value, 
+        dut.inp13.value, 
+        dut.inp14.value, 
+        dut.inp15.value, 
+        dut.inp16.value, 
+        dut.inp17.value, 
+        dut.inp18.value, 
+        dut.inp19.value,  
+        dut.inp20.value,
+        dut.inp21.value, 
+        dut.inp22.value, 
+        dut.inp23.value,
+        dut.inp24.value, 
+        dut.inp25.value, 
+        dut.inp26.value, 
+        dut.inp27.value, 
+        dut.inp28.value, 
+        dut.inp29.value, 
+        dut.inp30.value
+        ]
+
+        randomlist = random.randint(0,3,31)
+        inp = randomList
+        dut.sel.value = random.randint(0,31)
+        await Timer(2, units='ns')
+        
+        dut._log.info(f'A={A:05} B={B:05} model={A+B:05} DUT={int(dut.sum.value):05}')
+        assert dut.sum.value == A+B, "Randomised test failed with: {A} + {B} = {SUM}".format(
+            A=dut.a.value, B=dut.b.value, SUM=dut.sum.value)
     
 
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    
 
 
 
