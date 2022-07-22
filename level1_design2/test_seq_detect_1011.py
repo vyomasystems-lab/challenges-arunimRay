f222a298fb442a60941d6aeb15df220f2f5a8d05
arunimRay <arunimray06@gmail.com>
2022-07-22 14:51:48 +0000

    update test file

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -24,3 +24,15 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    for i in range(100): # 100 experiments
+        
+        exact = random.randint(0, 255) # generate randomized input
+        dut.d.value = exact # drive pins
+        
+        await FallingEdge(dut.clk) # wait for falling edge
+        
+        computed = dut.q.value.integer # Read pins as unsigned integer.
+        # computed = dut.q.value.signed_integer # Read pins as signed integer.
+        
+        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}" # If any assertion fails, the test fails, and the string would be printed in console
+        print(f"Driven value: {exact} \t received value: {computed}")
